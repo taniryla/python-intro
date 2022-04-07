c86a55c8b14f89b6afb498c091b56718ee4b3e2a
taniryla <ryland0000@gmail.com>
2022-04-07 07:57:39 +0100

    2nd  commit

--- intro.py
+++ intro.py
@@ -1,14 +1,13 @@
-money = 1000
-interest_rate = 1.15
-n = 5
-
-after_five_years = (money * interest_rate ** n) - 500
-
-
-def after_five_years(n):
-    (money * interest_rate ** n) - 500
-
-
-after_ten_years = after_five_years(10)
-
-print(after_ten_years)
+money = 100
+swimming = False
+job = True
+if (swimming == True):
+    money = money + 100
+else:
+    money = money - 35
+ 
+print(money)
+ 
+if job:
+    if (money >= 2500):
+        print('Walrus purchased. Life goal achieved.') 
